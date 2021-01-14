44edbd3dde4d3330ae4af3460044de0cb4158200
Travis Council <travis@councilstudios.com>
2021-01-14 00:23:05 -0700

    added left and right input

--- js/game.js
+++ js/game.js
@@ -51,22 +51,66 @@ let gameScene = new Phaser.Class({
 
         function gameScene() {
             Phaser.Scene.call(this, { key: 'gameScene' });
+
+            // Our player variables
+            this.alive = true;
+            this.speed = 250;
+            this.direction = 'LEFT';
         },
+
 
     preload: function () {
         this.load.image('bg', 'images/titleScreen.png');
+
+        this.load.image('title', 'images/title.png');
+        this.load.image('player', 'images/sprites/player_still_crossarmed.png');
     },
 
     create: function () {
         this.add.image(400, 300, 'bg');
+        this.player = this.physics.add.image(400, 300, 'player', 0);
+        this.add.text(320, 150, `Wander The Shadows`);
+
+        //  Create our keyboard controls
+        cursors = this.input.keyboard.createCursorKeys();
     },
 
     update: function (time, delta) {
+        this.player.setVelocity(0);
+
+        if (cursors.left.isDown) {
+            console.log('Hello from the Left Key!');
+            let direction = 'LEFT';
+            this.move(direction);
+        }
+        else if (cursors.right.isDown) {
+            console.log('Hello from the Right Key!');
+            let direction = 'RIGHT';
+            this.move(direction);
+        }
+        else if (cursors.up.isDown) {
+            console.log('Hello from the Up Key!');
+        }
+        else if (cursors.down.isDown) {
+            console.log('Hello from the Down Key!');
+        }
+
 
         /* End Game
         console.log('From gameScene to endScene');
         this.scene.start('endScene');
         */
+    },
+
+    move: function (direction){
+        if(direction === 'LEFT'){
+            this.player.setVelocityX(-this.speed);
+            this.player.flipX = true;
+        }
+        else if(direction === 'RIGHT'){
+            this.player.setVelocityX(this.speed);
+            this.player.flipX = false;
+        }
     }
 
 });
@@ -95,7 +139,16 @@ let config = {
     height: 600,
     backgroundColor: '#000000',
     parent: 'phaser-example',
-    scene: [titleScene, gameScene, endScene]
+    scene: [titleScene, gameScene, endScene],
+    physics: {
+      default: 'arcade',
+      arcade: {
+        debug: true,
+        gravity: {
+          y:5,
+        },
+      },
+    },
 };
 
 let game = new Phaser.Game(config);
