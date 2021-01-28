8ce06a7fb859975808a4ac93fb874cf8cae0ab52
Travis Council <travis@councilstudios.com>
2021-01-28 16:56:56 -0700

    orbs and levels

--- js/game.js
+++ js/game.js
@@ -53,6 +53,9 @@ let gameScene = new Phaser.Class({
             this.direction = 'STANDING';
             this.inAir = false;
             this.jumpReleased = true;
+            this.score = 0;
+            this.level = 1;
+            this.insight = 0;
         },
 
     preload: function () {
@@ -60,6 +63,7 @@ let gameScene = new Phaser.Class({
         this.load.image('title', 'images/title.png');
         this.load.image('player', 'images/sprites/player_still_crossarmed.png');
         this.load.image('platform', 'images/sprites/platform.png');
+        this.load.image('orb', 'images/sprites/insight.png');
     },
 
     platformCreator: function () {
@@ -79,7 +83,28 @@ let gameScene = new Phaser.Class({
         };
     },
 
+    orbsCreator: function () {
+        return {
+            key: 'orb',
+            repeat: 100, // Number of platforms
+            setXY: {
+                x: -1000, // Starting Point
+                y: 436,
+                stepX: 80, // Offset direction
+                stepY: 0
+            },
+            setScale: {
+                x: 0.5,
+                y: 0.5
+            }
+        };
+    },
+
+
     create: function () {
+
+
+
         // Player Create
         this.player = this.physics.add.sprite(0, 450, 'player');
         this.player.setBounce(0.05);
@@ -99,6 +124,27 @@ let gameScene = new Phaser.Class({
 
         this.physics.add.collider(this.player, this.platforms);
 
+        this.orbs = this.physics.add.staticGroup(this.orbsCreator());
+        Phaser.Actions.Call(this.orbs.getChildren(), orb => {
+            orb.setPipeline('Light2D');
+            orb.refreshBody();
+        }, this);
+
+
+        //  Using the Scene Data Plugin we can store data on a Scene level
+        this.data.set('level', this.level);
+        this.data.set('insight', this.insight);
+        this.data.set('score', this.score);
+
+        text = this.add.text(100, 100, '', { font: '12px Courier', fill: '#00ff00' });
+
+        text.setText([
+            'Level: ' + this.data.get('level'),
+            'Insight: ' + this.data.get('insight'),
+            'Score: ' + this.data.get('score')
+        ]).setScrollFactor(0);
+
+
         //  Create our keyboard controls
         cursors = this.input.keyboard.createCursorKeys();
         aKeyObj = this.input.keyboard.addKey('A');
@@ -114,10 +160,22 @@ let gameScene = new Phaser.Class({
 
     update: function (time, delta) {
         // Set player collision
-        // let playerRect = this.player.getBounds();
+        let playerRect = this.player.getBounds();
 
         // Handle player movement
         this.playerHandler();
+
+        Phaser.Actions.Call(this.orbs.getChildren(), orb => {
+
+            let orbRect = orb.getBounds();
+            if (Phaser.Geom.Intersects.RectangleToRectangle(playerRect, orbRect)) {
+                this.insight += 1;
+                this.score += 5;
+                orb.destroy();
+            }
+
+        }, this);
+
 
         if (this.player.y > 850) {
             this.gameOver();
@@ -161,12 +219,33 @@ let gameScene = new Phaser.Class({
             this.direction = "STANDING";
         }
 
+        // Update player stats and check for level up conditions
+        this.stats();
 
+        // Apply movement
         this.move(this.direction);
 
         // Set Light Position
         this.playerLight.x = this.player.x;
         this.playerLight.y = this.player.y;
+    },
+
+    stats: function () {
+        this.data.set('level', this.level);
+        this.data.set('insight', this.insight);
+        this.data.set('score', this.score);
+
+        text.setText([
+            'Level: ' + this.data.get('level'),
+            'Insight: ' + this.data.get('insight'),
+            'Score: ' + this.data.get('score')
+        ]).setScrollFactor(0);
+
+        this.cameras.main.zoom = 1 - (this.level * 0.01);
+        this.level = Math.ceil(this.insight / 50 );
+        this.playerLight.setRadius(200+(50*this.level)).setIntensity(1+this.level);
+        this.speed = 340 + (this.level * 10);
+
     },
 
     move: function (direction) {
@@ -188,9 +267,11 @@ let gameScene = new Phaser.Class({
     },
 
     gameOver: function () {
+        text.setText("");
         this.cameras.main.fade(750);
         this.cameras.main.on('camerafadeoutcomplete', function (camera, effect) {
             this.playerLight.setIntensity(0);
+
             this.scene.stop();
             this.scene.start('endScene');
         }, this);
@@ -208,7 +289,6 @@ let endScene = new Phaser.Class({
         },
 
     preload: function () {
-        this.load.image('bg', 'images/titleScreen.png');
     },
 
     create: function () {
